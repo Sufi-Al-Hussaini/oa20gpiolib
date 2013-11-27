8cc2f3b29b9e9db8fc5123772d5a57fb903611a2
Philippe Van Hecke <lemouchon@gmail.com>
2013-11-27 15:10:05 +0000

    change sample file to better handle SIGINT and do sunxi clean up

--- sample/gpio-test.c
+++ sample/gpio-test.c
@@ -1,26 +1,40 @@
 #include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+
+
 #include "../gpio/oa20_gpio.h" 
+
+
+void sig_handler(int signo)
+{
+  if (signo == SIGINT){
+       printf("handle SIGINT\n");
+       sunxi_gpio_cleanup();
+       exit(1);
+     }
+
+    
+}
 
 inline void msleep(long int t)
 {
    usleep(t*1000);
 }
+
 void main(void)
 {
-int i=0;
-long int t=0;
-if (SETUP_OK==sunxi_gpio_init())
-{
-  oa20_gpio_set_cfgpin(3,7,OUTPUT);
-  while (1){
-   if (i==0) i=1;
-   else i=0;
-   oa20_gpio_output(3,7,i);
-   msleep(1000
-);
-   printf("set =%d\n",i);
+ int i=0;
+ long int t=0;
+ if (SETUP_OK==sunxi_gpio_init()){
+    if (signal(SIGINT, sig_handler) == SIG_ERR) 
+       exit(1);
+    oa20_gpio_set_cfgpin(3,7,OUTPUT);
+    while (1){
+          i=(i+1)%2;
+          oa20_gpio_output(3,7,i);
+          msleep(1000);
+          printf("set =%d\n",i);
   }
-  sunxi_gpio_cleanup();
-}
-
+ }
 }
